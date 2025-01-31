96da0339abb8ab2b8e68633dac581ac9c479cd8a
amelies08 <amelie.sarrazin2@gmail.com>
2025-01-31 15:24:57 -0500

    Exercice_6

---
+++ Exercice_6.py
@@ -0,0 +1,4 @@
+nombre = input("Entrez un nombre : ")
+nombre = int(nombre) #le nombre était du type str au lieu du type int, donc il faut changer le type de varible
+resultat = nombre * 2
+print("Le double de votre nombre est :", resultat)
