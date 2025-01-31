e50cdae5e4bcf0b4c342ffba32da07e52b791ead
amelies08 <amelie.sarrazin2@gmail.com>
2025-01-31 15:57:42 -0500

    Exercice_8

---
+++ Exercice_8.py
@@ -0,0 +1,8 @@
+def addition(a, b):
+    resultat = a + b
+    return resultat
+
+x = 5 #la variable x était une variable de type str, au lieu de type int
+y = 3
+z = addition(x,y)
+print("Résultat:", z) 
