a8ade315505932a6ac1669e3ff327cb498d0ac55
amelies08 <amelie.sarrazin2@gmail.com>
2025-01-31 15:18:00 -0500

    Exercice_5

---
+++ Exercice_5.py
@@ -0,0 +1,5 @@
+"""Au départ, on demande si on a finit.
+    Si oui, c'est la fin.
+    Si non, on demande si la température est plus petite que 20 degrés celsius.
+        Si oui, il faut allumer le chauffage pendant 10 secondes, puis retourner à la question du départ.
+        Si non, il faut retourner à la question du départ."""
