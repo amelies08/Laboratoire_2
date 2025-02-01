3a9f045f5af95366b248ce8fe9acd4c5a6500b25
amelies08 <amelie.sarrazin2@gmail.com>
2025-01-31 21:12:40 -0500

    ExerciceBonusNombre

--- ExerciceBonusNombre.py
+++ ExerciceBonusNombre.py
@@ -1,7 +1,9 @@
 reponse = 34
 reponse = int(reponse)
+tentative = 1
 
 print("Devine le nombre entre 1 et 100.")
+print("nombre de tentatives : ", tentative)
 guess = input()
 guess = int(guess)
 
@@ -10,9 +12,13 @@ while guess != reponse :
         print("le nombre est trop grand")
         guess = input()
         guess = int(guess)
+        tentative = tentative + 1
+        print("nombre de tentatives : ", tentative)
     if guess < reponse :
         print("le nombre est trop petit")
         guess = input()
         guess = int(guess)
+        tentative = tentative + 1
+        print("nombre de tentatives : ", tentative)
 else:
     print("bravo!")
